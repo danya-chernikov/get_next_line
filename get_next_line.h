7e88c330f4c89aa9046da580a78f1049c07f6779
Daniil Chernikov <dchernik@c3r2s6.42urduliz.com>
2025-04-21 18:39:27 +0200

    Finally I made this stupid Norm passing

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_my.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dchernik <dchernik@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 19:48:46 by dchernik          #+#    #+#             */
-/*   Updated: 2025/04/21 16:55:46 by dchernik         ###   ########.fr       */
+/*   Updated: 2025/04/21 18:37:19 by dchernik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,38 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-# define EXIT		0
-# define READ		1
-# define ALLOC		2
-# define END		3
-# define AGAIN		4
+# define EXIT			0
+# define READ			1
+# define ALLOC			2
+# define END			3
+# define AGAIN			4
 
 /* FD		  - a file descriptor;
  * RLEN		  - bytes read from `fd` (FD);
  * I		  - an auxiliary coutner variable;
  * RES		  - result of a function execution;
  * PRIV_MEM_S - previously allocated memory size. */
-# define FD			0
-# define BUF_POS	1
-# define LINE_POS	2
-# define RLEN		3
-# define LINE_LEN	4
-# define I			5
-# define RES		6
-# define PRIV_MEM_S 7
+# define FD				0
+# define BUF_POS		1
+# define LINE_POS		2
+# define RLEN			3
+# define LINE_LEN		4
+# define I				5
+# define RES			6
+# define PRIV_MEM_S		7
 
 /* NORM  - normal execution. Means to do nothing;
  * RET   - indicate that the `line` must be returned;
  * BREAK - break from the main loop must be performed;
  * CONT  - continue must be applied on the main loop. */
-# define NORM		0
-# define RET		1
-# define BREAK		2
-# define CONT		3
+# define NORM			0
+# define RET			1
+# define BREAK			2
+# define CONT			3
 
 /* get_next_line.c */
 char	*get_next_line(int fd);
